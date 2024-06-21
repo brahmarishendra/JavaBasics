73bc9f4713313f184b6ceb07b726944fab525759
Brahmarishendra.K <121884148+brahmarishendra@users.noreply.github.com>
2024-06-21 22:56:41 +0530

    Update Area_of_triangle.java

--- Area_of_triangle.java
+++ Area_of_triangle.java
@@ -1,21 +1,18 @@
-//The area of a triangle
+
+//Area of the circle
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
+    Scanner pi = new Scanner(System.in);
+    System.out.println("Enter the radius of the circle: ");
+    double Pi = pi.nextDouble();
 
-      Scanner Base = new Scanner(System.in);
-      System.out.println("Enter the legnth of the rectangle: ");
+    Scanner radius = new Scanner(System.in);
 
-    int base = Base.nextInt();
-      //Wighth of the rectangle
-    Scanner Width = new Scanner(System.in);
-    System.out.println("Enter the width of the rectangle: ");
-    int width = Width.nextInt();
-    float area = (base * width) / 2.0f;
-    //printing out the area of the rectangle value
-    System.out.println("The area of the rectangle is " + area);
-
+    System.out.println("Enter the radius of the redius: ");
+    Double Radius = radius.nextDouble();
+    Double area = Math.PI * Radius * Radius;
+    System.out.println("The area of the circle is " + area);
   }
 }
